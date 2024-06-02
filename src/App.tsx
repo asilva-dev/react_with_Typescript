ab63db6a20cf2e1a519bd6b0ae1d4b58d49a733b
asilva-dev <amanda.adgti@gmail.com>
2024-06-02 14:21:38 -0300

    Fixing the InputComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
 import React from "react"
-import Button from "./Button";
 import Input from "./Input";
-// import Checkbox from "./Checkbox";
-
-function user(){
-  return {
-    nome:  'Amanda',
-    profissao: 'Software engineer'
-  }
-}
-
-type User = {
-  nome: string;
-  profissao: string;
-}
 
 type Venda = {
   id: string;
@@ -22,10 +8,9 @@ type Venda = {
 }
 
 function App() {
-  const [total, setTotal] = React.useState(0);
+  const [data, setData ] = React.useState<null | Venda[]>(null);
   const [inicio, setInicio] = React.useState("");
   const [final, setFinal] = React.useState("");
-  const [data, setData ] = React.useState<null | Venda[]>(null);
 
   React.useEffect(() => {
     if(inicio !== '' && final !== '')
@@ -34,14 +19,10 @@ function App() {
     .then((json) => setData(json as Venda[]))
     .catch((error) => console.log(error))
   },[inicio, final])
+  
   return (
-    <>
-      <div>
-        <p>
-        Total : {total}
-        </p>  
-        <Button incrementar={setTotal}/>   
-      </div>
+    <div>
+    
       <div>
         <Input label="Início" type="date" setState={setInicio} value={inicio} />
         <Input label="Final" type="date" setState={setFinal} value={final} />
@@ -55,7 +36,7 @@ function App() {
         ))
        }
       </ul>
-    </>
+    </div>
   )
 }
 
