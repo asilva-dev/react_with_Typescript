8ea0f8a2825480b197ceb3842e98e9747261d554
asilva-dev <amanda.adgti@gmail.com>
2024-06-05 20:29:34 -0300

    Create custom hook

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,14 +1,22 @@
 import React from "react"
 import videoSrc from "../assets/video.mp4";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const Video = () => {
     const [playing, setPlaying] = React.useState(false);
+    const [volume, setVolume] = useLocalStorage('volume', '0')
     const video = React.useRef<HTMLVideoElement>(null); 
 
     React.useEffect(() => {
         console.log(video.current)
     });
     
+    React.useEffect(() => {
+        if(!video.current) return;
+        const n = Number(volume);
+        if (n >= 0 && n <= 1) video.current.volume = n;
+    }, [volume])
+
     function forward(){
     if(!video.current) return
         video.current.currentTime += 2;
@@ -35,24 +43,31 @@ const Video = () => {
     return (
         <div >
             <div className="flex">
-        {playing ? (
-          <button onClick={() => video.current?.pause()}>Pause</button>
-        ) : (
-          <button onClick={() => video.current?.play()}>Play</button>
-        )}
-        <button onClick={forward}>+ 2s</button>
-        <button onClick={() => changePlayBackRate(1)}>1x</button>
-        <button onClick={() => changePlayBackRate(2)}>2x</button>
-        <button onClick={mute}>Mute</button>
-        <button onClick={pictureInPicture}>PiP</button>
-      </div>
-      <video
-       controls 
-       ref={video} 
-       src={videoSrc}
-       onPlay={() => setPlaying(true)}
-       onPause={() => setPlaying(false)}
-       ></video>
+                {playing ? (
+                <button onClick={() => video.current?.pause()}>Pause</button>
+                ) : (
+                <button onClick={() => video.current?.play()}>Play</button>
+                )}
+                <button onClick={forward}>+ 2s</button>
+                <button onClick={() => changePlayBackRate(1)}>1x</button>
+                <button onClick={() => changePlayBackRate(2)}>2x</button>
+                <button onClick={mute}>Mute</button>
+                <button onClick={pictureInPicture}>PiP</button>
+            </div>
+            <br />
+
+            <div className="flex">
+                <button onClick={() => setVolume('0')}>0</button>
+                <button onClick={() => setVolume('0.5')}>50</button>
+                <button onClick={() => setVolume('1')}>100</button>
+            </div>
+            <video
+            controls 
+            ref={video} 
+            src={videoSrc}
+            onPlay={() => setPlaying(true)}
+            onPause={() => setPlaying(false)}
+            ></video>
             
         </div>
     );
